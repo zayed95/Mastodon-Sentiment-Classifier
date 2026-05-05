6350bd3db16ce378545f7db37cf337a50764b246
Omneya Zayed <72omnia@gmail.com>
2026-05-05 23:42:13 +0300

    labeling logic

--- src/enums.py
+++ src/enums.py
@@ -6,3 +6,9 @@ class RepresentationMethod(Enum):
     GLOVE = "glove"
     TF_IDF = "tf_idf"
     WORD2VEC = "word2vec"
+
+class LLM(Enum):
+    
+    GPT4o_MINI = "openai/gpt-4o-mini"
+    GEMINI_FLASH = "google/gemini-2.5-flash"
+    CLAUDE_HAIKU = "anthropic/claude-3.5-haiku"
